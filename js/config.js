ab0687c75c283c82d769024ec2ad287d29394671
mesl <mesarwarlemon@gmail.com>
2026-02-19 22:50:04 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,7 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
 export const SUPABASE_URL = "https://nciahmkjmpowidzauzam.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_tnDIISrYfxJxvGUGhPm9CQ_sg3hyBb5";
+
+// Create and export supabase client
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
