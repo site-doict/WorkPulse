ccb98efe0630c73ac6b47b2035932489253c83b1
mesl <mesarwarlemon@gmail.com>
2026-02-21 18:40:59 +0600

    Delete js/super.js

--- js/super.js
+++
@@ -1,8 +0,0 @@
-// js/super.js
-import { supabase } from "./config.js";
-
-export async function loadAllUsers() {
-  const { data, error } = await supabase.from("profiles").select("*");
-  if (error) { console.error("loadAllUsers error:", error); return []; }
-  return data;
-}
